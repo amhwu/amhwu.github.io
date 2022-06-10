a6fbacf26ce81bd8e560e944789beaf919c1cfdd
Adrian <wucompile@gmail.con>
2022-06-10 10:34:10 +0100

    feat: update chome release

--- bizbingo/main.dart.js
+++ bizbingo/main.dart.js
@@ -49738,3 +49738,4 @@ a(b.target)}for(var r=0;r<s.length;++r)s[r].addEventListener("load",onLoad,false
 var s=A.XF
 if(typeof dartMainRunner==="function")dartMainRunner(s,[])
 else s([])})})()
+//# sourceMappingURL=main.dart.js.map
